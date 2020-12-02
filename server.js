3480706fef49722a339b623594b7bca9ebf02c37
arsalybek <askarbekovnav@gmail.com>
2020-12-02 23:34:56 +0600

    bugs

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/vue-interactive-paycard'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/vue-interactive-paycard/index.html'));});
-app.listen(process.env.PORT || 8080);
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use('/', serveStatic(path.join(__dirname,'/dist')))
+
+const port = process.env.PORT || 8080
+app.listen(port)
+
+console.log('Listening on port: ' + port)
